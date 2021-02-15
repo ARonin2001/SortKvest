3da3dded1538d4c915813e5811bdc0f8b70d5dfa
Alexandr <Aronin.2001@gmail.com>
2021-02-15 23:39:59 +0400

    add book IMG and hidden scroll for moon IMG

--- SportKvest/js/script.js
+++ SportKvest/js/script.js
@@ -1,19 +1,33 @@
 'use strict'
 
-let humanIMG = document.querySelector('.human_img'),
+let wrapp = document.querySelector('.wrapp'),
+    humanIMG = document.querySelector('.human_img'),
     human_msg = document.querySelector('.human_msg'),
     win = document.querySelector('.window'),
     overlay = document.querySelector('.overlay'),
     closeSpan = document.querySelector('.overlay__close'),
     overlayQuestion = document.querySelector('.overlay__question'),
-    answer = document.querySelectorAll('.overlay__answer');
+    answer = document.querySelectorAll('.overlay__answer'),
+    moon = document.querySelector('.moon__img'),
+    clock = document.querySelector('.moon__clock'),
+    bookIMG = document.querySelector('.book img');
 
 let questions = {
-    title: ["One", "Two", "Trhee"],
+    title: ["1. В каком году был изобретён баскетбол?", 
+    "2. ", 
+    "3. ", 
+    "4. ", 
+    "5. ", 
+    "6. Сколько шагов при ведении мяча разрешено в баскетболе?", 
+    "7. Сколько длится баскетбольный матч?"],
     answers: [
-        ["OneQ", "TwoQ", "ThreeQ", "nani"],
+        ["1900", "1891", "1872", "1723"],
         ["FourQ", "FiveQ", "SixQ", "nani"],
-        ["SevenQ", "EtchQ", "NineQ", "nani"]
+        ["SevenQ", "EtchQ", "NineQ", "nani"],
+        ["SevenQ", "EtchQ", "NineQ", "nani"],
+        ["SevenQ", "EtchQ", "NineQ", "nani"],
+        ["7", "3", "2", "Сколько угодно"],
+        ["1-2 часа", "1 час", "3 часа", "30 минут"]
     ]
 };
 
@@ -24,20 +38,33 @@ humanIMG.addEventListener('click', function() {
 overlay.addEventListener('click', function(event) {
     let target = event.target;
     if(target.classList.contains('overlay'))
-        overlay.classList.toggle('overlay_active');
+        overlayActive();
 });
 
 closeSpan.addEventListener('click', function() {
-    overlay.classList.toggle('overlay_active');  
+    overlayActive();
 });
 
 win.addEventListener('click', function() {
-    openCloseOverlay(1);
-    overlay.classList.toggle('overlay_active');
-    
+    openCloseOverlay(5);
+    overlayActive();
 });
 
-// показ overlay
+moon.addEventListener('click', function() {
+    moonActive();
+});
+
+clock.addEventListener('click', function() {
+    openCloseOverlay(6);
+    overlayActive();
+});
+
+bookIMG.addEventListener('click', function() {
+    openCloseOverlay(0);
+    overlayActive();
+});
+
+// вставка заголовка и ответов
 function openCloseOverlay(number) {
     let answerCount = 0;
     overlayQuestion.innerHTML = questions.title[number];
@@ -46,4 +73,17 @@ function openCloseOverlay(number) {
         answer[answerCount].innerHTML = item;
         answerCount++;
     });
+};
+function overlayActive() {
+    overlay.classList.toggle('overlay_active');
+};
+function moonActive() {
+    moon.style.cssText = "left: 150px; "+
+        "transform: rotate(120deg); "+
+        "transition: all .9s;";
+    setTimeout(moonDown, 700);
+};
+function moonDown() {
+    moon.style.top = "100vh";
+    setTimeout(() => moon.style.display = "none", 420);
 }
